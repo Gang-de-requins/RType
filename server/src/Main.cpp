d3028ffb37b34af50f71d299772c29116ca8778e
Tony Tran <tony@Tony>
2023-12-14 02:27:22 +0100

    [ADD] start rtype

--- server/src/Main.cpp
+++ server/src/Main.cpp
@@ -1,4 +1,18 @@
-int main(void)
+/*
+** EPITECH PROJECT, 2023
+** r type
+** File description:
+** Server
+*/
+
+#include "../include/ServerRooms.hpp"
+#include "Rtype.hpp"
+
+int main()
 {
-    return 0;
+	// serverGame::ServerRooms Rooms;
+	// Rooms.start();
+	serverGame::Rtype rtype(12345);
+	rtype.run();
+	return 0;
 }
