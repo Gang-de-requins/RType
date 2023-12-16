8b8a93f6887618ae6b414b19dec84bffa40ef837
Ethan SAUX <eowin.saux@gmail.com>
2023-12-16 10:44:37 +0100

    [ADD] comment for doxygen

--- ecs/include/Entity.hpp
+++ ecs/include/Entity.hpp
@@ -7,6 +7,19 @@
 #include <string_view>
 
 namespace ecs {
+    /**
+     * @struct Entity
+     * @brief Entity
+     * 
+     * @details
+     * This struct is used to store an entity.
+     * 
+     * @var Entity::id
+     * The id of the entity.
+     * 
+     * @var Entity::components
+     * The components of the entity.
+     */
     struct Entity {
         std::size_t id;
         std::unordered_map<std::string_view, std::any> components;
