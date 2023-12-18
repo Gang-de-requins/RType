ce618619e82139b1a93c613961c7d6c212afa9a7
Justin Thibault <justin.thibault@epitech.eu>
2023-12-18 22:43:00 +0100

    [ADD] added parallax.cpp systems

---
+++ ecs/src/systems/Parallax.cpp
@@ -0,0 +1,17 @@
+#include "systems/Parallax.hpp"
+#include "SceneManager.hpp"
+
+namespace ecs {
+    void ParallaxSystem::update(SceneManager &sceneManager) {
+        std::vector<Entities *> = sceneManager.view<Parallax, Position>(sceneManager.getCurrentScene());
+
+        for (auto &entity : entities) {
+            Parallax &parallax = sceneManager.get<Parallax>(*entity);
+            Position &position = sceneManager.get<Position>(*entity);
+
+            if (position.x <= -1920 && parallax.check == true) {
+                position.x = 0;
+            }
+        }
+    }
+}
