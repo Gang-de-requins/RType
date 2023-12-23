2acacb745f937ccacd0d4ed100239f16cc23a6fc
Justin Thibault <justin.thibault@epitech.eu>
2023-12-23 16:05:14 +0100

    [UPDATE] updated with doc doxygene in component scale

--- ecs/include/components/Scale.hpp
+++ ecs/include/components/Scale.hpp
@@ -2,6 +2,22 @@
 #define SCALE_COMPONENT_HPP
 
 namespace ecs {
+
+    /**
+     * @struct Scale
+     * @brief Scale component
+     * 
+     * @details
+     * This component is used to store and manage the scale of an entity in a 2D space. It holds scaling factors for the x and y axes, allowing for non-uniform scaling. The component includes operators for manipulating the scale.
+     * 
+     * @var Scale::x
+     * The scaling factor along the x-axis.
+     * 
+     * @var Scale::y
+     * The scaling factor along the y-axis.
+     * 
+     * The operators (+, +=, -, -=) are defined to facilitate the addition and subtraction of scale values, enabling easy modifications to an entity's scale.
+     */
     struct Scale {
         float x;
         float y;
