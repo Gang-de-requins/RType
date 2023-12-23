0f049435d91d6d956630423fb706aa53b15d721c
Justin Thibault <justin.thibault@epitech.eu>
2023-12-23 16:12:41 +0100

    [UPDATE] updated with doc doxygene in component vector2

--- ecs/include/components/Vector2.hpp
+++ ecs/include/components/Vector2.hpp
@@ -2,6 +2,20 @@
 #define VECTOR2_COMPONENT_HPP
 
 namespace ecs {
+
+    /**
+     * @struct Vector2
+     * @brief Vector2 component
+     * 
+     * @details
+     * This component represents a 2D vector and is used for various purposes such as defining positions, velocities, and sizes in a 2D space. It contains two float values representing the x and y components of the vector.
+     * 
+     * @var Vector2::x
+     * The x-component of the vector.
+     * 
+     * @var Vector2::y
+     * The y-component of the vector.
+     */
     struct Vector2 {
         float x;
         float y;
