d65aac666b73eff9191a286a7561754f176b4f41
Justin Thibault <justin.thibault@epitech.eu>
2023-12-23 16:11:51 +0100

    [UPDATE] updated with doc doxygene in component textinput

--- ecs/include/components/TextInput.hpp
+++ ecs/include/components/TextInput.hpp
@@ -1,14 +1,24 @@
 #ifndef TEXTINPUT_COMPONENT_HPP_
 #define TEXTINPUT_COMPONENT_HPP_
 
-    #include <string>
+#include <string>
 
 namespace ecs {
-    struct TextInput
-    {
+
+    /**
+     * @struct TextInput
+     * @brief TextInput component
+     * 
+     * @details
+     * This component is used to handle text input functionality for an entity. It can be used to store and manage user-entered text, such as in text fields or forms within the game or application.
+     * 
+     * @var TextInput::content
+     * The string that stores the current text input content. It represents the text entered by the user.
+     */
+    struct TextInput {
         std::string content;
     };
 
 }
 
-#endif /* !TEXTINPUT_HPP_ */
+#endif /* TEXTINPUT_COMPONENT_HPP_ */
