f91af5f614e0df1f83c369f63a6fb86110e36cb4
Justin Thibault <justin.thibault@epitech.eu>
2023-12-23 16:39:18 +0100

    [UPDATE] updated with doc doxygene in system music

--- ecs/include/systems/Music.hpp
+++ ecs/include/systems/Music.hpp
@@ -7,12 +7,27 @@
 #include "ISystem.hpp"
 
 namespace ecs {
-    class SceneManager;
 
+    /**
+     * @class MusicSystem
+     * @brief System responsible for handling music in the Entity Component System (ECS).
+     * 
+     * @details
+     * The MusicSystem class is a specific system designed to manage and update music-related components within the ECS framework.
+     */
     class MusicSystem : public ISystem {
-        public:
-            void update(SceneManager &sceneManager) override;
+    public:
+        /**
+         * @brief Update method for the MusicSystem.
+         * 
+         * @details
+         * This method is called to update the state of the MusicSystem. It should be implemented to handle music-related logic.
+         * 
+         * @param sceneManager Reference to the SceneManager managing the current scene and its entities.
+         */
+        void update(SceneManager &sceneManager) override;
     };
-}
+
+} // namespace ecs
 
 #endif /* MUSIC_SYSTEM_HPP */
