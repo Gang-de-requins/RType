5338dbab57ba93b05f27c9f5514f2b0b034a0bbc
Justin Thibault <justin.thibault@epitech.eu>
2023-12-23 15:59:27 +0100

    [UPDATE] updated with doc doxygene in component parallax

--- ecs/include/components/Parallax.hpp
+++ ecs/include/components/Parallax.hpp
@@ -9,10 +9,20 @@
 #define PARALLAX_COMPONENT_HPP
 
 namespace ecs {
+
+    /**
+     * @struct Parallax
+     * @brief Parallax component
+     * 
+     * @details
+     * This component is used to enable and manage parallax effect for an entity. Parallax effects create an illusion of depth in 2D scenes by moving background layers at different speeds.
+     * 
+     * @var Parallax::check
+     * A boolean that indicates whether the parallax effect is applied to the entity. If true, the entity will participate in the parallax effect.
+     */
     struct Parallax {
         bool check;
     };
 }
 
-#endif /* Parallax_COMPONENT_HPP */
-
+#endif /* PARALLAX_COMPONENT_HPP */
