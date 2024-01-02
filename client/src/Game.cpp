072612e506c84f1edc1d184ee2be866cbce0f6d8
Justin Thibault <justin.thibault@epitech.eu>
2024-01-02 20:18:33 +0100

    [ADD] added feature menu

--- client/src/Game.cpp
+++ client/src/Game.cpp
@@ -104,9 +104,22 @@ namespace rtype {
         >(inMenu);
 
 
+
+         ecs::Entity &Bakckground = this->m_world.createEntity(inMenu);
+        this->m_world.assign(Bakckground, ecs::Position{0, 0});
+        this->m_world.assign(Bakckground, ecs::Sprite{"assets/Menu/Space-Back-Menu.png", ecs::Rectangle{0, 0, 1920, 1080}, ecs::Vector2{0, 0}});
+        this->m_world.assign(Bakckground, ecs::Scale{1, 1});
+        this->m_world.assign(Bakckground, ecs::Rotation{0});
+
+        ecs::Entity &Title = this->m_world.createEntity(inMenu);
+        this->m_world.assign(Title, ecs::Position{350, 5});
+        this->m_world.assign(Title, ecs::Sprite{"assets/Menu/r-type.png", ecs::Rectangle{0, 0, 1258, 481}, ecs::Vector2{0, 0}});
+        this->m_world.assign(Title, ecs::Scale{1, 1});
+        this->m_world.assign(Title, ecs::Rotation{0});
+
         ecs::Entity &ButtonPlay = this->m_world.createEntity(inMenu);
-        this->m_world.assign(ButtonPlay, ecs::Position{500, 500});
-        this->m_world.assign(ButtonPlay, ecs::Sprite{"assets/buttonPlay.png", ecs::Rectangle{0, 0, 300, 153}, ecs::Vector2{0, 0}});
+        this->m_world.assign(ButtonPlay, ecs::Position{810, 300});
+        this->m_world.assign(ButtonPlay, ecs::Sprite{"assets/Menu/buttonPlay.png", ecs::Rectangle{0, 0, 300, 153}, ecs::Vector2{0, 0}});
         this->m_world.assign(ButtonPlay, ecs::Scale{1, 1});
         this->m_world.assign(ButtonPlay, ecs::Rotation{0});
         this->m_world.assign(ButtonPlay, ecs::Clickable{false, [this](ecs::Clickable&) {
@@ -131,6 +144,13 @@ namespace rtype {
         this->m_world.assign(PlayerTextInput, ecs::Color{200, 200, 200, 255});
         this->m_world.assign(PlayerTextInput, ecs::FontSize{50});
         this->m_world.assign(PlayerTextInput, ecs::TextColor{0, 0, 0, 255});
+
+        ecs::Entity &ButtonSettings = this->m_world.createEntity(inMenu);
+        this->m_world.assign(ButtonSettings, ecs::Position{810, 460});
+        this->m_world.assign(ButtonSettings, ecs::Sprite{"assets/Menu/buttonSettings.png", ecs::Rectangle{0, 0, 300, 153}, ecs::Vector2{0, 0}});
+        this->m_world.assign(ButtonSettings, ecs::Scale{1, 1});
+        this->m_world.assign(ButtonSettings, ecs::Rotation{0});
+        this->m_world.assign(ButtonSettings, ecs::Clickable{false, [this](ecs::Clickable&) {this->m_world.switchToScene(1);}});
 
         /* ------------------------- Scene InGame --------------------------------*/
         ecs::Scene &inGame = this->m_world.createScene();
