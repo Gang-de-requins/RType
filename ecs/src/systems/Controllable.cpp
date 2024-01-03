6dbc6431892666c1195c99d4214272fa1b06212c
Ethan SAUX <eowin.saux@gmail.com>
2024-01-03 17:10:02 +0100

    [DEL] old code in commentary

--- ecs/src/systems/Controllable.cpp
+++ ecs/src/systems/Controllable.cpp
@@ -40,30 +40,8 @@ namespace ecs {
                 });
             }
             if (IsKeyPressed(controllable.keySpace) && std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - controllable.shootUpdate).count() >= controllable.timeOut){
-                // auto entities = sceneManager.view<Sound>(sceneManager.getCurrentScene())[0];
-                // auto &sound = sceneManager.get<Sound>(*entities);
-                // auto &soundData = sceneManager.getSound(sound.path);
-
                 actionFound = true;
                 controllable.shootUpdate = std::chrono::steady_clock::now();
-                // PlaySound(soundData);
-                // Position &position = sceneManager.get<Position>(*entity);
-                // Sprite &sprite = sceneManager.get<Sprite>(*entity);
-                // Scale &scale = sceneManager.get<Scale>(*entity);
-                // Entity &Shoot = sceneManager.createEntity(sceneManager.getCurrentScene());
-                // sceneManager.assign(Shoot, Position{position.x + sprite.source.width *entity scale.x, position.y + (sprite.source.height / 2) * scale.y});
-                // sceneManager.assign(Shoot, Velocity{3, 0});
-                // sceneManager.assign(Shoot, Sprite{"assets/28.png", Rectangle{0, 0, 210, 92}, Vector2{0, 0}});
-                // sceneManager.assign(Shoot, Acceleration{0, 0, 15});
-                // sceneManager.assign(Shoot, Scale{0.25, 0.25});
-                // sceneManager.assign(Shoot, Rotation{0});;
-                // sceneManager.assign(Shoot, Collision{false, {}, true});
-                // sceneManager.assign(Shoot, Damage{10});
-
-                // scene.events[EventType::Input].push_back({
-                //     Event::Shoot,
-                //     {&Shoot}
-                // });
                 scene.events[EventType::Spawn].push_back({
                     Event::SpawnPlayerBullet,
                     {entity}
