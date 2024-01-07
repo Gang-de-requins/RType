9602e05c3f717c4c22a5fdd257b238b6d2dde947
Ethan SAUX <eowin.saux@gmail.com>
2024-01-07 15:11:03 +0100

    [UPDATE] copy binaries at root on windows

--- scripts/build_project.bat
+++ scripts/build_project.bat
@@ -7,5 +7,5 @@ cmake -S . -B build -D CMAKE_BUILD_TYPE=Release
 
 cmake --build build
 
-xcopy /s /y build\%PROJECT_CLIENT_DIR%\Debug\* .\bin\client\
-xcopy /s /y build\%PROJECT_SERVER_DIR%\Debug\* .\bin\server\
+xcopy /s /y build\%PROJECT_CLIENT_DIR%\Debug\* .
+xcopy /s /y build\%PROJECT_SERVER_DIR%\Debug\* .
