0fa71c0f68d5aaba4f7759f485c7c185348d2b50
Ethan SAUX <eowin.saux@gmail.com>
2024-01-07 22:48:32 +0100

    [UPDATE] files to copy for creating package for releases

--- scripts/build_project.bat
+++ scripts/build_project.bat
@@ -2,10 +2,15 @@
 
 set PROJECT_CLIENT_DIR=client
 set PROJECT_SERVER_DIR=server
+set RTYPE_CLIENT=r-type_client
+set RTYPE_SERVER=r-type_server
 
 cmake -S . -B build -D CMAKE_BUILD_TYPE=Release
 
 cmake --build build
 
-xcopy /s /y build\%PROJECT_CLIENT_DIR%\Debug\* .
-xcopy /s /y build\%PROJECT_SERVER_DIR%\Debug\* .
+copy /y build\%PROJECT_CLIENT_DIR%\Debug\%RTYPE_CLIENT%.exe .
+copy /y build\%PROJECT_CLIENT_DIR%\Debug\raylib.dll .
+
+copy /y build\%PROJECT_SERVER_DIR%\Debug\%RTYPE_SERVER%.exe .
+copy /y build\%PROJECT_SERVER_DIR%\Debug\raylib.dll .
