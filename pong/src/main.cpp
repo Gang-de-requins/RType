bc55d57d1625059ead08c5133d41c2968a279a5a
aco <alexandre.jankovic@epitech.eu>
2024-01-08 20:13:04 +0100

    [ADD] main of pong

---
+++ pong/src/main.cpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2024
+** RType
+** File description:
+** main
+*/
+
+#include <iostream>
+#include "Pong.hpp"
+
+int main(int ac, char **av)
+{
+    game::Pong pong;
+    std::cout << "Starting game..." << std::endl;
+
+    // while (true) {
+    //     pong.run();
+    // }
+}
