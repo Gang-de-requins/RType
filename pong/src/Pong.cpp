281e2fdcff3bba357d769c65520c3ccb69a87162
aco <alexandre.jankovic@epitech.eu>
2024-01-08 20:13:19 +0100

    [ADD] Pong.cpp with only run method for now

---
+++ pong/src/Pong.cpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2024
+** pong
+** File description:
+** Pong
+*/
+
+#include "Pong.hpp"
+
+game::Pong::Pong()
+{
+}
+
+game::Pong::~Pong()
+{
+}
+
+void game::Pong::run()
+{
+    std::cout << "Running game" << std::endl;
+}
