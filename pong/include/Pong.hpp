5319bf6417992b4183c09892ba53b5d493442474
aco <alexandre.jankovic@epitech.eu>
2024-01-08 20:01:58 +0100

    [ADD] basis of the 2nd game

---
+++ pong/include/Pong.hpp
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2024
+** RType
+** File description:
+** Pong
+*/
+
+#ifndef PONG_HPP__
+#define PONG_HPP__
+
+#include <iostream>
+#include "GameEngine.hpp"
+
+namespace game
+{
+
+    class Pong
+    {
+        public:
+            Pong();
+            ~Pong();
+            run();
+        
+            
+    };
+
+}
+
+#endif /* !PONG_HPP__ */
