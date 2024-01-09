57a2c12bc41389d183076f6caf5fb764adc90445
Tony Tran <tony.tran@epitech.eu>
2024-01-09 03:44:14 +0100

    [UPDATE] rtypeThread add

--- server/src/ServerRooms.cpp
+++ server/src/ServerRooms.cpp
@@ -50,12 +50,33 @@ void serverGame::ServerRooms::createRoom(std::string roomName)
 {
     std::shared_ptr<serverGame::Rtype> rtype = std::make_shared<serverGame::Rtype>(this->port);
     rtype->name = roomName;
+    this->threadPool.push_back(std::thread(rtypeThread, rtype));
+    // créer une liste pour stocker les infos pour pouvoir les envoyers aux clients
     this->port += 2;
 }
 
 void serverGame::ServerRooms::getRooms(boost::asio::ip::udp::endpoint endpoint)
 {
 
+}
+
+void receiveMessageThread(std::shared_ptr<serverGame::Rtype> rtype)
+{
+	while(true)
+	{
+		ecs::Buffer buffer;
+        rtype->server.receiveMessage(buffer);
+		rtype->mutex.lock();
+		rtype->bufferList.push_back(buffer);
+		rtype->mutex.unlock();
+	}
+}
+
+void sendGameStateThread(std::shared_ptr<serverGame::Rtype> rtype) {
+    while (true) {
+        rtype->sendGameState();
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000 / 32));
+    }
 }
 
 void rtypeThread(std::shared_ptr<serverGame::Rtype> rtype)
