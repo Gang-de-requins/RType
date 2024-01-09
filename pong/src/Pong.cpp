9d362ca3be8d4999d2392ff61dbea0c1b7a73e94
Alexandre <alexandre.jankovic@epitech.eu>
2024-01-09 11:27:17 +0100

    [ADD] Redid the assets of the menu, now looks better + Music added

--- pong/src/Pong.cpp
+++ pong/src/Pong.cpp
@@ -49,10 +49,6 @@ namespace game
         this->world.registerSystems<
             ecs::MusicSystem,
             ecs::ControllableSystem,
-            ecs::MovementSystem,
-            ecs::CollisionSystem,
-            ecs::LifeSystem,
-            ecs::ParallaxSystem,
             ecs::RenderSystem,
             ecs::ClickableSystem,
             ecs::TextSystem
@@ -60,7 +56,7 @@ namespace game
 
         /* Menu Text Entity */
         ecs::Entity &Pong = this->world.createEntity(inMenu);
-        this->world.assign(Pong, ecs::Position{650, 50});
+        this->world.assign(Pong, ecs::Position{800, 50});
         this->world.assign(Pong, ecs::Sprite{"../assets/pong.png", ecs::Rectangle{0, 0, 500, 500}, ecs::Vector2{0, 0}});
         this->world.assign(Pong, ecs::Scale{0.7, 0.7});
         this->world.assign(Pong, ecs::Rotation{0});
@@ -69,9 +65,9 @@ namespace game
         /* Player 1 Name Entity */
         ecs::Entity &NamePlayer1 = this->world.createEntity(inMenu);
 
-        this->world.assign(NamePlayer1, ecs::Position{500, 300});
+        this->world.assign(NamePlayer1, ecs::Position{625, 400});
         this->world.assign(NamePlayer1, ecs::Rectangle{0, 0, 300, 100});
-        this->world.assign(NamePlayer1, ecs::TextInput{10, ecs::Position{500, 325}, "Player1"});
+        this->world.assign(NamePlayer1, ecs::TextInput{10, ecs::Position{660, 425}, "Player1"});
         this->world.assign(NamePlayer1, ecs::Scale{1, 1});
         this->world.assign(NamePlayer1, ecs::Rotation{0});
         this->world.assign(NamePlayer1, ecs::Clickable{false, [this](ecs::Clickable&) {
@@ -89,9 +85,9 @@ namespace game
         /* Player 2 Name Entity */
         ecs::Entity &NamePlayer2 = this->world.createEntity(inMenu);
 
-        this->world.assign(NamePlayer2, ecs::Position{900, 300});
+        this->world.assign(NamePlayer2, ecs::Position{1025, 400});
         this->world.assign(NamePlayer2, ecs::Rectangle{0, 0, 300, 100});
-        this->world.assign(NamePlayer2, ecs::TextInput{10, ecs::Position{900, 325}, "Player2"});
+        this->world.assign(NamePlayer2, ecs::TextInput{10, ecs::Position{1060, 425}, "Player2"});
         this->world.assign(NamePlayer2, ecs::Scale{1, 1});
         this->world.assign(NamePlayer2, ecs::Rotation{0});
         this->world.assign(NamePlayer2, ecs::Clickable{false, [this](ecs::Clickable&) {
@@ -101,20 +97,30 @@ namespace game
 
             this->name2 = textInput.content;
             textInput.isFocused = true;
+
         }});
         this->world.assign(NamePlayer2, ecs::Color{200, 200, 200, 255});
         this->world.assign(NamePlayer2, ecs::FontSize{50});
         this->world.assign(NamePlayer2, ecs::TextColor{0, 0, 0, 255});
 
-
+        /* Play Button Entity */
         ecs::Entity &ButtonPlay = this->world.createEntity(inMenu);
-        this->world.assign(ButtonPlay, ecs::Position{500, 500});
-        this->world.assign(ButtonPlay, ecs::Sprite{"../assets/buttonPlay.png", ecs::Rectangle{0, 0, 300, 153}, ecs::Vector2{0, 0}});
-        this->world.assign(ButtonPlay, ecs::Scale{1, 1});
+        this->world.assign(ButtonPlay, ecs::Position{870, 700});
+        this->world.assign(ButtonPlay, ecs::Sprite{"../assets/p_play.png", ecs::Rectangle{0, 0, 100, 41}, ecs::Vector2{0, 0}});
+        this->world.assign(ButtonPlay, ecs::Scale{2, 2});
         this->world.assign(ButtonPlay, ecs::Rotation{0});
         this->world.assign(ButtonPlay, ecs::Clickable{false, [this](ecs::Clickable&) {
             std::cout << "ButtonPlay clicked" << std::endl;
+            std::cout << "Player1: " << this->name1 << std::endl << "Player2: " << this->name2 << std::endl;
             // this->world.switchToScene(1);
         }});
+
+        
+        /* Menu Music Entity */
+        ecs::Entity &MenuMusic = this->world.createEntity(inMenu);
+        this->world.assign(MenuMusic, ecs::Music({"../assets/pong_menu.wav", 0.1f, true}));
+
+
+
     }
 }
