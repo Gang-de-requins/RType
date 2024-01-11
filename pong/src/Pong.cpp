1400f43d9b3c4d8a7f812c6aee8426faac930043
Alexandre <alexandre.jankovic@epitech.eu>
2024-01-11 13:05:21 +0100

    [ADD] Pong ball is here, all the assets are good, need to make movements

--- pong/src/Pong.cpp
+++ pong/src/Pong.cpp
@@ -136,27 +136,39 @@ namespace game
             ecs::AnimationSystem,
             ecs::MovementSystem,
             ecs::SoundSystem
-        >(inMenu);
+        >(inGame);
 
         /* Map background */
         ecs::Entity &Background = this->world.createEntity(inGame);
-        this->world.assign(Pong, ecs::Position{0, 0});
+        this->world.assign(Pong, ecs::Position{65, 0});
         this->world.assign(Pong, ecs::Sprite{"../assets/pong_map.png", ecs::Rectangle{0, 0, 1920, 1080}, ecs::Vector2{0, 0}});
         this->world.assign(Pong, ecs::Scale{1, 1});
         this->world.assign(Pong, ecs::Rotation{0});
 
 
         ecs::Entity &p1 = this->world.createEntity(inGame);
-        this->world.assign(p1, ecs::Position{500, 500});
-        this->world.assign(p1, ecs::Sprite{"../assets/player1.png", ecs::Rectangle{0, 0, 74, 539}, ecs::Vector2{0, 0}});
-        this->world.assign(p1, ecs::Scale{1, 1});
+        this->world.assign(p1, ecs::Position{10, 375});
+        this->world.assign(p1, ecs::Sprite{"../assets/player1.png", ecs::Rectangle{0, 0, 50, 520}, ecs::Vector2{0, 0}});
+        this->world.assign(p1, ecs::Scale{0.5, 0.5});
         this->world.assign(p1, ecs::Rotation{0});
-
+        this->world.assign(p1, ecs::Controllable{KEY_Z, KEY_S});
+        this->world.assign(p1, ecs::Acceleration{0, 0, 8});
 
         ecs::Entity &p2 = this->world.createEntity(inGame);
-        this->world.assign(p2, ecs::Position{0, 0});
-        this->world.assign(p2, ecs::Sprite{"../assets/player2.png", ecs::Rectangle{0, 0, 67, 594}, ecs::Vector2{0, 0}});
-        this->world.assign(p2, ecs::Scale{1, 1});
+        this->world.assign(p2, ecs::Position{1885, 375});
+        this->world.assign(p2, ecs::Sprite{"../assets/player2.png", ecs::Rectangle{0, 0, 50, 520}, ecs::Vector2{0, 0}});
+        this->world.assign(p2, ecs::Scale{0.5, 0.5});
         this->world.assign(p2, ecs::Rotation{0});
+        this->world.assign(p2, ecs::Controllable{KEY_UP, KEY_DOWN});
+        this->world.assign(p2, ecs::Acceleration{0, 0, 8});
+
+
+        ecs::Entity &ball = this->world.createEntity(inGame);
+        this->world.assign(ball, ecs::Position{850, 450});
+        this->world.assign(ball, ecs::Sprite{"../assets/pong_ball.png", ecs::Rectangle{0, 0, 75, 75}, ecs::Vector2{0, 0}});
+        this->world.assign(ball, ecs::Scale{1, 1});
+        this->world.assign(ball, ecs::Rotation{0});
+        this->world.assign(ball, ecs::Controllable{KEY_UP, KEY_DOWN});
+        this->world.assign(ball, ecs::Acceleration{0, 0, 8});
     }
 }
