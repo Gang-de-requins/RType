8c8c0fe8ddc83e051404f1d788e876584ae231e3
Justin Thibault <justin.thibault@epitech.eu>
2024-01-11 23:22:22 +0100

    [UPDATE] updated with name field

--- client/src/LoadChooseName.cpp
+++ client/src/LoadChooseName.cpp
@@ -17,21 +17,37 @@ namespace rtype {
         this->m_world.assign(Bakckground_2, ecs::Scale{1, 1});
         this->m_world.assign(Bakckground_2, ecs::Rotation{0});
 
+        ecs::Entity &TextChooseName = this->m_world.createEntity(scene);
+        this->m_world.assign(TextChooseName, ecs::Position{300, 200});
+        this->m_world.assign(TextChooseName, ecs::TextInput{10, ecs::Position{300, 225}, "Choose your name !!"});
+        this->m_world.assign(TextChooseName, ecs::Scale{1, 1});
+        this->m_world.assign(TextChooseName, ecs::Rotation{0});
+        this->m_world.assign(TextChooseName, ecs::FontSize{50});
+        this->m_world.assign(TextChooseName, ecs::TextColor{255, 255, 255, 255});
+
+        ecs::Entity &test = this->m_world.createEntity(scene);
+        this->m_world.assign(test, ecs::Position{355, 300});
+        this->m_world.assign(test, ecs::TextInput{10, ecs::Position{355, 325}, "=>"});
+        this->m_world.assign(test, ecs::Scale{1, 1});
+        this->m_world.assign(test, ecs::Rotation{0});
+        this->m_world.assign(test, ecs::FontSize{50});
+        this->m_world.assign(test, ecs::TextColor{255, 255, 255, 255});
+
         ecs::Entity &PlayerTextInput = this->m_world.createEntity(scene);
-        this->m_world.assign(PlayerTextInput, ecs::Position{500, 300});
-        this->m_world.assign(PlayerTextInput, ecs::Rectangle{0, 0, 300, 100});
-        this->m_world.assign(PlayerTextInput, ecs::TextInput{10, ecs::Position{500, 325}});
+        this->m_world.assign(PlayerTextInput, ecs::Position{400, 300});
+        this->m_world.assign(PlayerTextInput, ecs::Rectangle{0, 0, 300, 85});
+        this->m_world.assign(PlayerTextInput, ecs::TextInput{10, ecs::Position{400, 325}});
         this->m_world.assign(PlayerTextInput, ecs::Scale{1, 1});
         this->m_world.assign(PlayerTextInput, ecs::Rotation{0});
         this->m_world.assign(PlayerTextInput, ecs::Clickable{false, [this](ecs::Clickable&) {
-            ecs::Entity &PlayerTextInput = this->m_world.getEntityById(this->m_world.getCurrentScene(), 1);
+            ecs::Entity &PlayerTextInput = this->m_world.getEntityById(this->m_world.getCurrentScene(), 3);
             ecs::TextInput &textInput = this->m_world.get<ecs::TextInput>(PlayerTextInput);
 
             textInput.isFocused = true;
         }});
         this->m_world.assign(PlayerTextInput, ecs::Color{200, 200, 200, 255});
         this->m_world.assign(PlayerTextInput, ecs::FontSize{50});
-        this->m_world.assign(PlayerTextInput, ecs::TextColor{255, 255, 255, 255});
+        this->m_world.assign(PlayerTextInput, ecs::TextColor{0, 0, 0, 255});
         this->m_world.assign(PlayerTextInput, ecs::Submit{KEY_ENTER, [this](ecs::SceneManager &sceneManager, ecs::Entity *entity) {
             if (sceneManager.has<ecs::TextInput>(*entity)) {
                 ecs::TextInput &textInput = sceneManager.get<ecs::TextInput>(*entity);
