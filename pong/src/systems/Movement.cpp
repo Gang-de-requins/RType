ab39034b8a6d3a093aed8a80ae37fdc69af17a5f
aco <alexandre.jankovic@epitech.eu>
2024-01-12 18:56:09 +0100

    [ADD] new system for pong

---
+++ pong/src/systems/Movement.cpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2024
+** RType
+** File description:
+** Controllable
+*/
+
+#include "systems/Movement.hpp"
+#include "SceneManager.hpp"
+
+namespace ecs
+{
+
+    void MovementSystem::update(SceneManager& sceneManager)
+    {
+        return;
+    }
+
+    void MovementSystem::move(SceneManager& sceneManager, Entity *entity)
+    {
+        return;
+    }
+}
