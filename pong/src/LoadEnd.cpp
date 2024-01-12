8ccdefc85f410633d0dfdb9f3504d02b55c7ced7
aco <alexandre.jankovic@epitech.eu>
2024-01-12 18:57:36 +0100

    [ADD] LoadEnd scene

---
+++ pong/src/LoadEnd.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2024
+** RType
+** File description:
+** LoadEnd
+*/
+
+#include "Pong.hpp"
+
+namespace game
+{
+    void Pong::LoadEnd(ecs::Scene &scene)
+    {
+        return;
+    }
+}
