adf3e100af20d376311ad6159ed2d9e6ff91c192
Hugo MARTINS GANDRA <hugo.martins-gandra@epitech.eu>
2024-01-13 23:25:05 +0100

    [FIX] code for better understanding

--- server/include/Message.hpp
+++ server/include/Message.hpp
@@ -10,18 +10,22 @@ namespace server {
         public:
             Message() {}
             ~Message() {}
+
             void setMessageType(MessageType messageType)
             {
                 this->_messageType = messageType;
             }
+
             void setMessage(std::string msg)
             {
                 this->_msg = msg;
             }
+
             MessageType getMessageType() const
             {
                 return this->_messageType;
             }
+
             std::string getMessage() const
             {
                 return this->_msg;
