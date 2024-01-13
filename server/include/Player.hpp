4910a85875ffe8604d164e7cb413bced4af8a6e6
Hugo MARTINS GANDRA <hugo.martins-gandra@epitech.eu>
2024-01-13 23:30:00 +0100

    [ADD] Player getName to handle usernames

--- server/include/Player.hpp
+++ server/include/Player.hpp
@@ -10,6 +10,8 @@ namespace server {
             Player(std::string name, asio::ip::udp::endpoint endpoint) : _name(name), _endpoint(endpoint) {}
             ~Player() {}
 
+            std::string getName() const { return _name; }
+
         private:
             std::string _name;
     };
