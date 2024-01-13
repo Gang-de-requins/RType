de1c80e557284bf594215b55e6ebd640781ac3bb
Hugo MARTINS GANDRA <hugo.martins-gandra@epitech.eu>
2024-01-13 22:34:49 +0100

    [ADD] WIP new server with less problems with new ecs

--- server/src/Server.cpp
+++ server/src/Server.cpp
@@ -1,62 +1,40 @@
-/*
-** EPITECH PROJECT, 2023
-** r type
-** File description:
-** Server
-*/
+#include "Server.hpp"
 
-#include <iostream>
-#include "../include/Server.hpp"
-#include "Network.hpp"
-#include <iomanip>
-#include "Message.hpp"
-#include "GameEngine.hpp"
+namespace server {
+    Server::Server(short port)
+    {
+        setup(port);
+        this->_world = ecs::World();
 
-serverGame::Server::Server()
-{
-}
+        ecs::Scene scene = this->_world.createScene();
 
-serverGame::Server::~Server()
-{
+        this->_world.registerSystems<
+            ecs::MovementSystem,
+            ecs::CollisionSystem,
+            ecs::LifeSystem
+        >(scene);
 
-}
+        ecs::Entity &ParallaxBack1 = this->_world.createEntity(scene);
 
-void serverGame::Server::setupServer(int port)
-{
-    this->endpoint = boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), boost::asio::ip::port_type(port));
-    this->socket = std::make_shared<boost::asio::ip::udp::socket>(this->service, this->endpoint);
-}
+        this->_world.assign(ParallaxBack1, ecs::Position{0, 0});
+        this->_world.assign(ParallaxBack1, ecs::Velocity{-3, 0});
+        this->_world.assign(ParallaxBack1, ecs::Sprite{"assets/space-back.png", ecs::Rectangle{0, 0, 7596, 1080}, ecs::Vector2{0, 0}});
+        this->_world.assign(ParallaxBack1, ecs::Parallax{true});
+        this->_world.assign(ParallaxBack1, ecs::Acceleration{0, 0, 3});
+        this->_world.assign(ParallaxBack1, ecs::Scale{1, 1});
+        this->_world.assign(ParallaxBack1, ecs::Rotation{0});
 
+        ecs::Entity &ParallaxBack2 = this->_world.createEntity(scene);
 
-void serverGame::Server::receiveMessage(ecs::Message &message) {
-    std::vector<char> buffer(1024);
-    boost::asio::ip::udp::endpoint sender_endpoint;
-
-    std::size_t length = this->socket->receive_from(boost::asio::buffer(buffer), sender_endpoint);
-
-    if (length >= sizeof(std::size_t) + sizeof(ecs::MessageType)) {
-        std::size_t dataSize;
-        ecs::MessageType messageType;
-        std::memcpy(&dataSize, buffer.data(), sizeof(dataSize));
-        std::memcpy(&messageType, buffer.data() + sizeof(dataSize), sizeof(messageType));
-
-        message.setMessageType(messageType);
-
-        if (length >= sizeof(dataSize) + sizeof(messageType)) {
-            std::vector<char> messageData(buffer.begin() + sizeof(dataSize) + sizeof(messageType), 
-                                            buffer.begin() + sizeof(dataSize) + sizeof(messageType) + dataSize);
-            message.setMessageData(messageData);
-        }
-        message.setEndpoint(sender_endpoint);
+        this->_world.assign(ParallaxBack2, ecs::Position{1920, 0});
+        this->_world.assign(ParallaxBack2, ecs::Velocity{-1.5, 0});
+        this->_world.assign(ParallaxBack2, ecs::Sprite{"assets/far-planets.png", ecs::Rectangle{0, 0, 272, 160}, ecs::Vector2{0, 0}});
+        this->_world.assign(ParallaxBack2, ecs::Parallax{true});
+        this->_world.assign(ParallaxBack2, ecs::Acceleration{0, 0, 2});
+        this->_world.assign(ParallaxBack2, ecs::Scale{1920 / 272, 1080 / 160});
+        this->_world.assign(ParallaxBack2, ecs::Rotation{0});
     }
-}
 
-// void serverGame::Server::sendMessage(ecs::Buffer& buffer, const boost::asio::ip::udp::endpoint& recipientEndpoint) {
-//     try {
-//         const std::vector<char>& sendBuffer = buffer.getData();
-
-//         socket->send_to(boost::asio::buffer(sendBuffer), recipientEndpoint);
-//     } catch (std::exception& e) {
-//         std::cerr << "Exception in sendMessage: " << e.what() << std::endl;
-//     }
-// }
+    Server::~Server()
+    {
+    }
