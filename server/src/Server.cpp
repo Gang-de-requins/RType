bbd0164d22974fde1a2665787778fe21b1d2d8f7
Hugo MARTINS GANDRA <hugo.martins-gandra@epitech.eu>
2024-01-13 22:37:38 +0100

    [ADD] server start for basic connection

--- server/src/Server.cpp
+++ server/src/Server.cpp
@@ -38,3 +38,48 @@ namespace server {
     Server::~Server()
     {
     }
+
+    void Server::setup(short port)
+    {
+        _endpoint = asio::ip::udp::endpoint(asio::ip::udp::v4(), asio::ip::port_type(port));
+        _socket = std::make_shared<asio::ip::udp::socket>(_io_context, _endpoint);
+    }
+
+    void Server::run()
+    {
+        const frame tickDuration(1);
+
+        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+        std::chrono::time_point<std::chrono::steady_clock> fpsTimer = std::chrono::steady_clock::now();
+        frame FPS{};
+
+        while (true) {
+            FPS = std::chrono::duration_cast<frame>(std::chrono::steady_clock::now() - fpsTimer);
+
+            if (FPS >= tickDuration) {
+                this->_world.update();
+            }
+
+            processMessages();
+
+            if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - begin).count() > 2000 && _players.size() > 1) {
+                std::cout << "Spawning enemy" << std::endl;
+
+                begin = std::chrono::steady_clock::now();
+
+                std::string tag = "enemy" + std::to_string(rand() % 1000);
+
+                EntityTemplate entityTemplate = EntityTemplate();
+                entityTemplate.enemy(this->_world, tag, 900, 100);
+
+                for (auto &player : _players) {
+                    ecs::Buffer buffer;
+                    buffer.writeMessageType(ecs::MessageType::ENEMY_SPAWN);
+                    std::string response = tag + ":900:100";
+                    buffer.writeString(response);
+                    send(buffer, player.getEndpoint());
+                }
+            }
+        }
+    }
