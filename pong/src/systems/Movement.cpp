dd7bb0904b39ffdb703418e4427a8de7578a4103
Alexandre <alexandre.jankovic@epitech.eu>
2024-01-14 15:36:42 +0100

    [ADD] Movement feature for pong (ecs)

--- pong/src/systems/Movement.cpp
+++ pong/src/systems/Movement.cpp
@@ -1,23 +1,29 @@
 /*
 ** EPITECH PROJECT, 2024
-** RType
+** RType [WSL : Ubuntu]
 ** File description:
-** Controllable
+** Movement
 */
 
 #include "systems/Movement.hpp"
 #include "SceneManager.hpp"
+#include "Events.hpp"
 
 namespace ecs
 {
 
-    void MovementSystem::update(SceneManager& sceneManager)
+    void MovementSystem::update(SceneManager &sceneManager)
     {
-        return;
-    }
+        ecs::Scene& scene = sceneManager.getCurrentScene();
+        std::vector<Entity*> list = sceneManager.view<ecs::Position, ecs::Velocity>(scene);
 
-    void MovementSystem::move(SceneManager& sceneManager, Entity *entity)
-    {
-        return;
+
+        for (auto &entity : list) {
+            ecs::Position &actualPos = sceneManager.get<ecs::Position>(*entity);
+            ecs::Velocity &actualVel = sceneManager.get<ecs::Velocity>(*entity);
+
+            actualPos.x += actualVel.dx;
+            actualPos.y += actualVel.dy;
+        }
     }
 }
