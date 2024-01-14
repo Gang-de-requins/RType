c74bbfb56a7825bd493983018c9fd3e37519ccc8
Alexandre <alexandre.jankovic@epitech.eu>
2024-01-14 15:37:45 +0100

    [ADD] new scene : loadend

--- pong/src/LoadEnd.cpp
+++ pong/src/LoadEnd.cpp
@@ -11,6 +11,30 @@ namespace game
 {
     void Pong::LoadEnd(ecs::Scene &scene)
     {
-        return;
+        this->world.registerSystems<
+            ecs::MusicSystem,
+            ecs::TextSystem,
+            ecs::RenderSystem,
+            ecs::ClickableSystem
+        >(scene);
+
+        ecs::Entity &Winner = this->world.createEntity(scene);
+        this->world.assign(Winner, ecs::Position{800, 50});
+        this->world.assign(Winner, ecs::Text({this->winner}));
+        this->world.assign(Winner, ecs::FontSize({25}));
+        this->world.assign(Winner, ecs::TextColor({255, 255, 255, 255}));
+
+
+
+        ecs::Entity &ButtonBack = this->world.createEntity(scene);
+        this->world.assign(ButtonBack, ecs::Position{725, 700});
+        this->world.assign(ButtonBack, ecs::Sprite{"../assets/pong_back.png", ecs::Rectangle{0, 0, 245, 60}, ecs::Vector2{0, 0}});
+        this->world.assign(ButtonBack, ecs::Scale{2, 2});
+        this->world.assign(ButtonBack, ecs::Rotation{0});
+        this->world.assign(ButtonBack, ecs::Clickable{false, [this](ecs::Clickable&) {
+            std::cout << "BackButton pressed" << std::endl;
+            // LoadGame(this->world.getSceneById(3));
+            // this->world.switchToScene(3);
+        }});
     }
 }
